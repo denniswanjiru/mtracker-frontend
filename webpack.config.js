5c8af8d4ae6f1e8be804914227f7d06c771723ae
denniswanjiru <denniswanjiru71@gmail.com>
2018-06-18 10:13:21 +0300

    [Refactor] Change the file structure of the app

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   mode: "development",
   entry: {
-    app: "./src/js/app.js"
+    app: "./src/app.js"
   },
   output: {
     path: path.resolve(__dirname, "public"),
