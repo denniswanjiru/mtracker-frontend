67de410a8bd99fda787aaef9b027cd6dc96be85e
denniswanjiru <denniswanjiru71@gmail.com>
2018-06-18 11:40:01 +0300

    [Chore #158420470] Build out post method

--- src/components/api.js
+++ src/components/api.js
@@ -2,9 +2,21 @@ const baseUrl = "https://m-tracker-app.herokuapp.com/api/v2";
 
 export const get = (endpoint, token) => {
   return fetch(`${baseUrl}${endpoint}`, {
+    method: "GET",
     headers: {
       Authorization: `Bearer ${token}`,
       "content-type": "application/json"
     }
   }).then(res => res.json());
+};
+
+export const post = (endpoint, data, token) => {
+  return fetch(`${baseUrl}${endpoint}`, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "content-type": "application/json"
+    }
+  });
 };
